3240fa80ab74426d882ef63e2a515a9b3ad3b666
Abhij12 <abhikris128@gmail.com>
2024-08-25 14:52:55 +0530

    Add files via upload

---
+++ ArrayOperations/Test.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+#include<Integer_array_operation.h> //include header file
+
+void main() {
+
+   // you can use this function after copying the header files in your include folder in turboc 
+   // Array_1d();
+   // Array_2d();
+   // bubble();
+   // bubble_circle();
+    getch();
+}
